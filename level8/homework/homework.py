cbe1a81c66dff094c9a64d623b7b8fe5c0ddafd0
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2024-06-08 17:49:12 +0400

    first commit

---
+++ level8/homework/homework.py
@@ -0,0 +1,35 @@
+#Task 1
+# text1=2
+# text2=5
+# print(text1 != text2)#True
+# print(text1 <= text2)#True
+# print(text1 >= text2)#False
+
+
+
+# number1=21
+# number2=90
+
+# print(number1 == number2)#False
+# print(number1 != number2)#True
+# print(number1 <= number2)#True
+# print(number1 >= number2)#False
+# print(number1 < number2)#True
+# print(number1 > number2)#False
+# print(5 < 2)#False
+# print(5 > 2)#True
+
+
+
+#and da or operatorebis wesebi:magalitad rodesac vprintavt 5 < 2 es gamoitans False radgan mcdaria esaa "And" 
+#xolo rodesac 5 > 2 es gamoitans true radgan Wesmaritia  esaa "Or"
+
+
+
+num1=input(f"Enter first number:25 ")
+num2=input(f"Enter second number:23")
+
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
