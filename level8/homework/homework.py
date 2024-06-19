902a0016c4fbc16d514fd36d83e926463ced6ece
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2024-06-20 01:07:25 +0400

    first commit

--- level8/homework/homework.py
+++ level8/homework/homework.py
@@ -1,35 +1,36 @@
 #Task 1
-# text1=2
-# text2=5
-# print(text1 != text2)#True
-# print(text1 <= text2)#True
-# print(text1 >= text2)#False
+text1=2
+text2=5
+print(text1 != text2)#True
+print(text1 <= text2)#True
+print(text1 >= text2)#False
 
 
 
-# number1=21
-# number2=90
+#Task 2
+number1=21
+number2=90
 
-# print(number1 == number2)#False
-# print(number1 != number2)#True
-# print(number1 <= number2)#True
-# print(number1 >= number2)#False
-# print(number1 < number2)#True
-# print(number1 > number2)#False
-# print(5 < 2)#False
-# print(5 > 2)#True
+print(number1 == number2)#False
+print(number1 != number2)#True
+print(number1 <= number2)#True
+print(number1 >= number2)#False
+print(number1 < number2)#True
+print(number1 > number2)#False
+print(5 < 2)#False
+print(5 > 2)#True
 
 
-
+#Task 3
 #and da or operatorebis wesebi:magalitad rodesac vprintavt 5 < 2 es gamoitans False radgan mcdaria esaa "And" 
 #xolo rodesac 5 > 2 es gamoitans true radgan Wesmaritia  esaa "Or"
 
 
-
+#Task 4
 num1=input(f"Enter first number:25 ")
 num2=input(f"Enter second number:23")
 
 
-print(num1 > num2)
-print(num1 < num2)
-print(num1 == num2)
+print(num1 > num2)#False
+print(num1 < num2)#False
+print(num1 == num2)#True
