3e67c0b31d546cb0a930c9401e8263fb62c979b4
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2024-06-22 00:14:41 +0400

    First commit

---
+++ level10/homework/homework.py
@@ -0,0 +1,29 @@
+# for i in range(101):
+#     print(i)
+
+
+# i=1
+# while i < 1001:
+#     print(i)
+#     i+=1
+
+
+
+
+
+
+
+# i=-9
+# while i + 10:
+#      print(i)
+#      i+=1
+
+
+
+
+t=0
+n=1
+while n < 10:
+    t+=n
+    n+=1
+    print("The sum of numbers from 1 to 10 is:",t)
