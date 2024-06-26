def52eac0292be6e1fabd7016455be39a59cc463
giorgimaisuradze21 <maisuradzegio40@gmail.com>
2024-06-26 21:50:44 +0400

    first commit

--- level10/homework/homework.py
+++ level10/homework/homework.py
@@ -1,29 +1,36 @@
-# for i in range(101):
-#     print(i)
+# 2) for loopის გამოყენებით გამოიტანეთ რიცხვები 1დან 100ის ჩათვლით 2ის გამოტოვებით.
+for i in range(1,101,2):
+    print(i)
 
 
-# i=1
-# while i < 1001:
-#     print(i)
-#     i+=1
+# 3) while loopის გამოყენებით გამოიტანეთ რიცხვები 1დან 1000მდე.
+
+number = 1
+while number < 1001:
+    print(number)
+    number += 1
+
+# 4) while loopის გამოყენებით გამოიტანეთ 1დან 10მდე რიცხვების ჯამი.
+
+number = 0
+sum = 0
+while number < 10:
+    sum += number
+    number += 1
+print(sum)
 
 
 
+# 5) ითამაშეთ ამ ორი loopით, გააკეთეთ 5 for loop მაგალითი და 5 while loop მაგალითი (ისეთი რაც აქამდე არ გაგიკეთებიათ).
 
+for i in range(101):
+    print(i)
 
+for i in range(1,101,2):
+    print(i)
 
+for i in range(11):
+    print(str(i) + "." + "hello world")
 
-# i=-9
-# while i + 10:
-#      print(i)
-#      i+=1
-
-
-
-
-t=0
-n=1
-while n < 10:
-    t+=n
-    n+=1
-    print("The sum of numbers from 1 to 10 is:",t)
+for i in range(11):
+    print("hello world")
